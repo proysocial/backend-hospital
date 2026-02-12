ebe38c4a3766634bcaf06fec271dc745d7139086
Favio-coder <faviusam@gmail.com>
2026-02-12 05:10:55 -0500

    Creación de admin

--- hospital/hospital/urls.py
+++ hospital/hospital/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import path, include
+from django.urls import include
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/v1/', include("medireport.urls")),
     # Refrescar y solicitar Token
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
